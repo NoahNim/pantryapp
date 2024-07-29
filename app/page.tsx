84123d2e0701a48211efec9e6f0213a8d2ae08a7
Noah Medoff <noahmedoff@gmail.com>
2024-07-29 16:01:26 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Box, Stack, List, ListItem } from "@mui/material";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+export default function Home() {
+  return (
+    <Box 
+    width="100%" 
+    height="100%"
+    display={"flex"}
+    justifyContent={"center"}
+    alignItems={"center"}
+    >
+    </Box>
+  );
+}
